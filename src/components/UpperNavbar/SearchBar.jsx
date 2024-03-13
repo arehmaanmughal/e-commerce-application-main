939398222199fb65a980ece3741c739a80faa4f3
Abdulrehman <arehmaanmughal@gmail.com>
2024-03-13 09:33:12 +0500

    upper nav

--- src/components/UpperNavbar/SearchBar.jsx
+++ src/components/UpperNavbar/SearchBar.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import { IoIosSearch } from "react-icons/io";
 import "./UpperNavbar.css";
-import { Button, Form, InputGroup } from "react-bootstrap";
+import { Button, Col, Form, Row } from "react-bootstrap";
 const SearchBar = () => {
   return (
     <>
-      <div >
-        <Form >
-          <InputGroup className="border rounded-pill d-flex align-items-center ">
-          <Form.Select >
-          <optgroup className="border rounded">
-          <option>All Category</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-          </optgroup>
-              
-            </Form.Select>
-            <span className="text-secondary">|</span>
-            <Form.Control
+      <Form className="">
+        <Row className="mx-0 border rounded-pill align-items-center ">
+          <Col xs={4} className="">
+            <select className="text-secondary">
+              <optgroup>
+                <option>All Category</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </optgroup>
+            </select>
+          </Col>
+
+          {/* <span className="text-secondary">|</span> */}
+          <Col xs={6} className="">
+            <input
+              className="text-secondary"
               placeholder="What you looking for?"
               type="text"
             />
-            
-            <Button className="btn rounded-pill text-white d-flex align-items-center justify-content-center" variant="outline-secondary">
-              <IoIosSearch/>
+          </Col>
+
+          <Col xs={2} className="">
+            <Button className="rounded-pill text-white d-flex align-items-center justify-content-center">
+              <IoIosSearch />
             </Button>
-          </InputGroup>
-        </Form>
-      </div>
+          </Col>
+        </Row>
+      </Form>
     </>
   );
 };
